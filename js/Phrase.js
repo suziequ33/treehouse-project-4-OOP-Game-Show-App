c2f09529bbc0af37d3c7d8b21f1ba05ee854030d
Suzanna Mitchell <suziequ33@gmail.com>
2023-06-11 23:35:21 -0500

    editing and adding comments

--- js/Phrase.js
+++ js/Phrase.js
@@ -9,34 +9,16 @@ class Phrase{
 
 addPhraseToDisplay() {
     const phraseList = document.querySelector('#phrase ul');
-    //const ul = phraseDiv.querySelector('ul');
-    //ul.innerHTML = '';
     let phraseHTML = '';
-   // phraseList.innerHTML = '';
-
+ 
     for (let i = 0; i < this.phrase.length; i++) {
-        //const li = document.createElement('li');
+      
         if(this.phrase[i] !== ' ') {
-           // li.className = `hide letter ${this.phrase[i]}`;
-           // li.textContent = this.phrase[i];
             phraseHTML += `<li class="hide letter ${this.phrase[i]}">${this.phrase[i]}</li>`;
         } else {
-            //li.className = 'space';
             phraseHTML += '<li class="space"></li>';
-        }
-        //phraseList.appendChild(li);
-       // const letter = this.phrase[i];
-        //const li = document.createElement('li');
-        //li.textContent = letter;
-        
-       // if( letter !== ' ') {
-         //   li.textContent = letter;
-           // li.classList.add('hide', 'letter', letter);
-       // } else {
-        //    li.classList.add('space');
-       // }
+    }
     
-       // ul.appendChild(li);
     }
     phraseList.innerHTML = phraseHTML;
 }
@@ -44,30 +26,19 @@ checkLetter(letter) {
     return this.phrase.includes(letter);
     }
 showMatchedLetter(letter) {
-    //const matchedLetters = document.querySelectorAll(`.${letter}`);
-    const letters = document.querySelectorAll('.letter');
-    letters.forEach(phraseLetter => {
+    const phraseLetters = document.querySelectorAll('.letter');
+    phraseLetters.forEach(phraseLetter => {
         if(phraseLetter.textContent === letter) {
             phraseLetter.classList.remove('hide');
             phraseLetter.classList.add('show');
     }
-   // matchedLetters.forEach((matchedLetter) => {
-       // matchedLetter.classList.remove('hide');
-       // matchedLetter.classList.add('show');
-    });
+ 
+ });
 
-    //const phraseDiv = document.querySelector('#phrase');
-    //const lis = phraseDiv.querySelectorAll('li');
-
-   // lis.forEach((li) => {
-       // if (li.textContent === letter) {
-          //  li.classList.remove('hide');
-           // li.classList.add('show');
-       // }
-   // });
 }
 
 }
 
 
-/**found .includes in notes section 3, (String Manipulation with JavaScript/Search Methods) */
+/**Used template literals for the empty boxes.  
+ * found .includes in notes section 3, (String Manipulation with JavaScript/Search Methods) */
--- js/app.js
+++ js/app.js
@@ -5,22 +5,11 @@
 const game = new Game();
 
 document.getElementById('btn__reset').addEventListener('click', () => {
-   // game = new Game();
     game.startGame();
 });
-//const keyboard = document.querySelector('#qwerty');
 
 document.querySelector('#qwerty').addEventListener('click', (event) => {
-    //if(event.target.tagName === 'BUTTON' && event.target.classList.contains('key')) {
         if(event.target.className ==='key') {
         game.handleInteraction(event.target);
     }
 });
-//document.addEventListener('keydown', (e) => {
-   // if(/[a-z]/i.test(e.key)) {
-      //  const keyButton = document.querySelector(`button.key:enabled[data-key='${e.key}']`);
-   // if(keyButton) {
-       // game.handleInteraction(keyButton);
-   // }
-// }
-//});
